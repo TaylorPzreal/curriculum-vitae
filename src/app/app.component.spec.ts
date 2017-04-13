fbef471dd23f8710d75ed70d31aea0cca4f739a1
TaylorPzreal <taylorpzreal@gmail.com>
2017-04-14 07:28:21 +0800

    更新tslint.json配置

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,7 +5,7 @@ describe('App', () => {
     TestBed.configureTestingModule({ declarations: [AppComponent]});
   });
   it ('should work', () => {
-    let fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppComponent);
     expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
 @NgModule({
   imports: [
     BrowserModule
