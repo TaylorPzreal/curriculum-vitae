01ccf8f7b4b51054a1f555a1eb7565ee528d1a7b
TaylorPzreal <taylorpzreal@gmail.com>
2017-04-22 10:08:35 +0800

    添加路由:home,login

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+
+// 引入核心模块
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login';
+import { HomeComponent } from './home';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', component: HomeComponent},
+  { path: '', redirectTo: '', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
-  MdButtonModule, MdCheckboxModule, MdToolbarModule, MdIconModule, MdMenuModule, MdProgressSpinnerModule
+  MdButtonModule,
+  MdCheckboxModule,
+  MdToolbarModule,
+  MdIconModule,
+  MdMenuModule,
+  MdProgressSpinnerModule,
+  MdCardModule,
+  MdInputModule
 } from '@angular/material';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { LoginComponent } from './login';
+import { HomeComponent } from './home';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MdButtonModule,
     MdCheckboxModule,
     MdToolbarModule,
     MdIconModule,
     MdMenuModule,
-    MdProgressSpinnerModule
+    MdProgressSpinnerModule,
+    MdCardModule,
+    MdInputModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   bootstrap: [ AppComponent ]
 })
