93ab0ac5cd307d0448b53a1a8837c9b31f6358fc
TaylorPzreal <taylorpzreal@gmail.com>
2017-05-06 07:28:11 +0800

    完善D3展示

--- src/app/datav/datav.component.ts
+++ src/app/datav/datav.component.ts
@@ -1,5 +1,8 @@
 import * as d3 from 'd3';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'datav',
@@ -15,19 +18,106 @@ export class DatavComponent implements OnInit {
   }
 
   public initKnowledge(): void {
-    console.warn('777');
 
     // SVG 图形大小
     const width = document.body.clientWidth;
     const height = document.body.clientHeight;
 
-    const knowledgeDom: Element = document.querySelector('#knowledge');
-
-    const svg = d3.select('#knowkedge')
-      .attr('width', width)
+    const svg = d3.select('#knowledge').append('svg')
+      .attr('width', width - 120)
       .attr('height', height);
 
-      // let g = svg.append('g').attr('transform', `translate(${width / 2 + 40}, ${height / 2 + 90})`);
+    const g = svg.append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+    const tree = d3.tree()
+      .size([360, 500])
+      .separation((a, b) => {
+        return (a.parent === b.parent ? 1 : 2) / a.depth;
+      });
+
+    const JsonData = {
+      name: '我的大前端',
+      children: [{
+        name: 'JavaScript',
+        children: [{
+          name: 'jQuery'
+        }, {
+          name: 'Angularjs'
+        }, {
+          name: 'Angular'
+        }]
+      }, {
+        name: 'HTML5',
+        children: [{
+            name: 'SVG'
+          },
+          {
+            name: 'Canvas'
+          }
+        ]
+      }, {
+        name: 'CSS3',
+        children: [{
+          name: 'Sass'
+        }, {
+          name: 'postCss'
+        }]
+      }]
+    };
+
+    // const stratify = d3.stratify()
+    //     .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+
+    const root = d3.hierarchy(JsonData);
+    // root.sum((d) => (d.value)).sort((a, b) => b.value - a.value);
+    tree(root);
+
+    function project(x: number, y: number) {
+      const angle = (x - 90) / 180 * Math.PI;
+      const radius = y;
+      return [radius * Math.cos(angle), radius * Math.sin(angle)];
+    }
+
+    // const link = g.selectAll('.link')
+    //   .data(root.descendants().slice(1))
+    //   .enter().append('path')
+    //   .attr('class', 'link')
+    //   .attr('d', (d) => {
+    //     return 'M' + project(d.x, d.y) +
+    //       'C' + project(d.x, (d.y + d.parent.y) / 2) +
+    //       ' ' + project(d.parent.x, (d.y + d.parent.y) / 2) +
+    //       ' ' + project(d.parent.x, d.parent.y);
+    //   });
+
+
+    const node = g.selectAll('.node')
+      .data(root.descendants())
+      .enter().append('g')
+      .attr('class', (d) => {
+        return 'node' + (d.children ? ' node--internal' : ' node--leaf');
+      })
+      .attr('transform', (d) => {
+        return 'translate(' + project(d.x, d.y) + ')';
+      });
+
+    node.append('circle')
+      .attr('r', 2.5);
+
+    node.append('text')
+      .attr('dy', '.31em')
+      .attr('x', (d) => {
+        return d.x < 180 === !d.children ? 6 : -6;
+      })
+      .style('text-anchor', (d) => {
+        return d.x < 180 === !d.children ? 'start' : 'end';
+      })
+      .attr('transform', (d) => {
+        return 'rotate(' + (d.x < 180 ? d.x - 90 : d.x + 90) + ')';
+      })
+      .text((d) => {
+        return d.data.name;
+      });
   }
 
 }
