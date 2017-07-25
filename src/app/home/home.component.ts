e42e6360c12846409b16b8bd16e1386199ee9806
taylorpzreal <taylorpzreal@gmail.com>
2017-07-25 19:53:05 +0800

    add images lazyload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { HomeService } from './home.service';
 import { Article } from './article';
+import 'lazysizes'; // 图片懒加载
 
 interface IMovie {
   id: number;
