0068a48791e291be265c835f9ebf48d172a54dd2
taylorpzreal <taylorpzreal@taylorpaldeiMac.lan>
2017-08-12 16:40:29 +0800

    update conf

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
+
+declare const process: any;
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
