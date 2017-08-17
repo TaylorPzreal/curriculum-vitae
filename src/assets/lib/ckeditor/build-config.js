a2f32f718966df71996d0c9cb49a468858a3bbb3
taylorpzreal <taylorpzreal@taylorpaldeiMac.lan>
2017-08-17 08:43:46 +0800

    add ckeditor

---
+++ src/assets/lib/ckeditor/build-config.js
@@ -0,0 +1,111 @@
+﻿/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/e4e040c0c2683e933110f0c584c31289
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/e4e040c0c2683e933110f0c584c31289
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono-lisa',
+	preset: 'basic',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'basicstyles' : 1,
+		'bidi' : 1,
+		'blockquote' : 1,
+		'brclear' : 1,
+		'chart' : 1,
+		'clipboard' : 1,
+		'codeTag' : 1,
+		'codesnippet' : 1,
+		'colorbutton' : 1,
+		'colordialog' : 1,
+		'contextmenu' : 1,
+		'copyformatting' : 1,
+		'docfont' : 1,
+		'elementspath' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'filebrowser' : 1,
+		'find' : 1,
+		'fixed' : 1,
+		'floatingspace' : 1,
+		'font' : 1,
+		'format' : 1,
+		'horizontalrule' : 1,
+		'htmlwriter' : 1,
+		'image' : 1,
+		'image2' : 1,
+		'imagepaste' : 1,
+		'imageresize' : 1,
+		'indentblock' : 1,
+		'indentlist' : 1,
+		'justify' : 1,
+		'letterspacing' : 1,
+		'lineheight' : 1,
+		'link' : 1,
+		'list' : 1,
+		'markdown' : 1,
+		'mathjax' : 1,
+		'pastecode' : 1,
+		'pastefromexcel' : 1,
+		'pastefromword' : 1,
+		'pbckcode' : 1,
+		'preview' : 1,
+		'removeformat' : 1,
+		'sourcedialog' : 1,
+		'table' : 1,
+		'tableresize' : 1,
+		'templates' : 1,
+		'textselection' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'uploadimage' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'en' : 1,
+		'zh-cn' : 1
+	}
+};
